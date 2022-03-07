8f13c4a146b9312a4e0e27c2cff4450cb1b3fc83
yuanxin11044 <yuanxin11044@gmail.com>
2022-03-07 12:39:09 +0800

    initial commit

---
+++ src/sonarlint/sonarlintClass.java
@@ -0,0 +1,14 @@
+package sonarlint;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class sonarlintClass {
+	@Test
+	public void assertTrueTest() {
+		assertTrue(1 == 1);
+		assertTrue("FirstName".startsWith("F"));
+	} 
+
+}
